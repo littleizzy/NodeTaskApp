894378d1b80831e18abb70fa3bbe5fc0bfa74561
Izzy Zhao <izzy_zxy@hotmail.com>
2018-10-13 19:08:07 -0700

    final versions w/o mysql

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var app = express();
 
+
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/style', express.static(__dirname + '/style'));
+
+
+app.get('/', function(req, res){
+  res.sendFile('home.html', {'root': __dirname + '/templates'});
+})
+
+app.get('/signin', function(req, res){
+  res.sendFile('signin.html', {'root': __dirname + '/templates'});
+})
+
+app.get('/signup', function(req, res){
+  res.sendFile('signup.html', {'root': __dirname + '/templates'});
+})
+
 app.listen(3000, function(){
   console.log('App listening on port 3000...');
 });
